a942b1db4a30b45f57cee5d7a719f0ed4f1de459
Silver5478 <bryanboh@gmail.com>
2018-07-25 12:22:37 +0800

    #Fixed

--- app/src/main/java/sg/edu/np/s10171537connect/madassignment/Constant.java
+++ app/src/main/java/sg/edu/np/s10171537connect/madassignment/Constant.java
@@ -10,7 +10,7 @@ public class Constant {
     static int numQuestion;
     static List QFanswerlist;
     static Question question;
-    static List<Player> PlayerList;
+    static List<Player> PlayerList = new ArrayList<Player>();
     static String catergory;
 
     static public void AddData() {
